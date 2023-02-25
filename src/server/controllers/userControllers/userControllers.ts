0c4e323e957b4e19e4c3227fa90004fe20d87535
David Botello <dabocher@gmail.com>
2023-02-25 13:44:41 +0100

    Create userController

---
+++ src/server/controllers/userControllers/userControllers.ts
@@ -0,0 +1,20 @@
+import CustomError from "../../../customError/customError.js";
+import type { Request, Response, NextFunction } from "express";
+import User from "../../../database/models/Users.js";
+
+const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await User.find().exec();
+
+    res.status(200).json({ users });
+  } catch (error) {
+    const customError = new CustomError(
+      error.message as string,
+      500,
+      "Couldn't retrieve users"
+    );
+    throw customError;
+  }
+};
+
+export default getUsers;
